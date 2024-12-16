3da6c563b46df843a921e15823ad0c01f73f2a51
YUSEOK_SON <95211040+sysyuseok@users.noreply.github.com>
2024-12-16 13:53:01 +0900

    Update Form1.cs

--- WindowsFormsApp_241213/Form1.cs
+++ WindowsFormsApp_241213/Form1.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp_241213
+namespace WindowsFormsApp_241216
 {
     public partial class Form1 : Form
     {
@@ -16,11 +9,37 @@ namespace WindowsFormsApp_241213
         {
             InitializeComponent();
 
-            //MessageBox.Show("Hello, World!");
-            textBox_print.Text = "hi\r\nhihi";
-            int a = 10;
-            
-
+            byte ageOfOldestManInEarth=100;
+            short increaseDecreasePopulationPerNation=10000;
+            int hairLossIncomePerDay=1000000000;
+            float degreeOfClockHands = 37.628f;
+            double militaryServiceRateForEverySecond = 15.32321542;
+            decimal cellNumInBody = 10000000000000000;
+            textBox_print.Text += ageOfOldestManInEarth.GetType();
+            textBox_print.Text += "   ageOfOldestManInEarth   ";
+            textBox_print.Text += ageOfOldestManInEarth.ToString();
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += increaseDecreasePopulationPerNation.GetType();
+            textBox_print.Text += "   increaseDecreasePopulationPerNation   ";
+            textBox_print.Text += increaseDecreasePopulationPerNation.ToString();
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += hairLossIncomePerDay.GetType();
+            textBox_print.Text +="   hairLossIncomePerDay   ";
+            textBox_print.Text += hairLossIncomePerDay.ToString();
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += degreeOfClockHands.GetType();
+            textBox_print.Text +="   degreeOfClockHands   ";
+            textBox_print.Text += degreeOfClockHands.ToString();
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += militaryServiceRateForEverySecond.GetType();
+            textBox_print.Text +="   militaryServiceRateForEverySecond   ";
+            textBox_print.Text += militaryServiceRateForEverySecond.ToString();
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += cellNumInBody.GetType();
+            textBox_print.Text += "   cellNumInBody   ";
+            textBox_print.Text += cellNumInBody.ToString();
+            textBox_print.Text += "\r\n";
         }
+
     }
 }
